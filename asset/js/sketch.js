7f56e773e0191161a053f758b8ed750c61728cd0
Elliot Thiebaut <60610988+ElliotThiebaut@users.noreply.github.com>
2020-02-24 11:42:50 +0100

    Basic design

---
+++ asset/js/sketch.js
@@ -0,0 +1,78 @@
+var bar = document.getElementById('js-progressbar');
+    
+UIkit.upload('.js-upload', {
+
+    url: '',
+    multiple: false,
+
+    beforeSend: function () {
+        console.log('beforeSend', arguments);
+    },
+    beforeAll: function () {
+        console.log('beforeAll', arguments);
+    },
+    load: function () {
+        console.log('load', arguments);
+    },
+    error: function () {
+        console.log('error', arguments);
+    },
+    complete: function () {
+        console.log('complete', arguments);
+    },
+
+    loadStart: function (e) {
+        console.log('loadStart', arguments);
+
+        bar.removeAttribute('hidden');
+        bar.max = e.total;
+        bar.value = e.loaded;
+    },
+
+    progress: function (e) {
+        console.log('progress', arguments);
+
+        bar.max = e.total;
+        bar.value = e.loaded;
+    },
+
+    loadEnd: function (e) {
+        console.log('loadEnd', arguments);
+
+        bar.max = e.total;
+        bar.value = e.loaded;
+    },
+
+    completeAll: function () {
+        console.log('completeAll', arguments);
+
+        setTimeout(function () {
+            bar.setAttribute('hidden', 'hidden');
+        }, 1000);
+
+        alert('Upload Completed');
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Initialize the Image Classifier method with MobileNet
+// ml5.imageClassifier('MobileNet')
+//   .then(classifier => classifier.classify(image))
+//   .then(results => {
+//     result.innerText = results[0].label;
+//     probability.innerText = results[0].confidence.toFixed(4);
+//   });
